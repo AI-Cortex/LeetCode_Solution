3c3a6580855b15b9fabb40719bc97e30459ddbc3
AI Cortex <artificial.intelligence.cortex@gmail.com>
2025-06-14 08:33:31 +0330

    add code
    - Binary Search.cpp

--- README.md
+++ README.md
@@ -1385,6 +1385,30 @@
     </td>
 </tr>
 
+<!-- 58 __________________________________________________________________________________________________ -->
+
+<tr>
+    <td>
+        58
+    </td>
+    <td>
+        Binary Search
+    </td>
+    <td>
+        704
+    </td>
+    <td>
+        <a href="https://leetcode.com/problems/binary-search/" target="_blank">
+            problem
+        </a>
+    </td>
+    <td>
+        <a href="https://github.com/AI-Cortex/LeetCode_Solution/blob/main/code%20c%2B%2B/Binary%20Search.cpp" target="_blank">
+            solution c++
+        </a>
+    </td>
+</tr>
+
 <!-- . __________________________________________________________________________________________________ -->
 
 <tr>
---
+++ code c++/Binary Search.cpp
@@ -0,0 +1,27 @@
+// my profile : https://leetcode.com/u/AI-Cortex/
+// Leetcode solution github repository : https://github.com/AI-Cortex/LeetCode_Solution
+//////////////////////////////////////////////////////////////////////
+// name : Binary Search
+// link : https://leetcode.com/problems/binary-search/
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int start = 0, end = nums.size() - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            else if (nums[mid] < target)
+                start = mid + 1;
+
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
+};
